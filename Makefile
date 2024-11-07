65208165c9c337e5dc4afa7a56e615fb98428b42
Necio Veras <necioveras@gmail.com>
2024-11-07 17:33:29 -0300

    Repositorio template para exercicio da matriz dinamica

---
+++ Makefile
@@ -0,0 +1,25 @@
+saida.out:
+	gcc main.c
+
+clean:
+	rm a.out
+
+test-all: test1 test2 test3 test4 test5 test6 clean
+
+test1: saida.out
+	bash ./testes/test1.sh
+
+test2: saida.out
+	bash ./testes/test2.sh
+
+test3: saida.out
+	bash ./testes/test3.sh
+
+test4: saida.out
+	bash ./testes/test4.sh
+
+test5: saida.out
+	bash ./testes/test5.sh
+
+test6: saida.out
+	bash ./testes/test6.sh
---
+++ testes/test4.sh
@@ -0,0 +1,24 @@
+echo "Testando (7 linhas e 1 coluna)..."
+echo
+
+output=$(./a.out < ./testes/input4.txt)
+expected_output="59\n83\n78\n23\n51\n2\n78"
+
+if [ $? -eq 0 ] ; then
+  echo "Running ok: Program exited zero"
+else
+  echo "Fail on running: Program did not exit zero"
+  exit 1
+fi
+
+if [ "$output" == "$expected_output" ] ; then
+  echo "Pass: A saida esperada esta correta"
+else
+  echo "Era esperado '$expected_output' mas o programa retornou: $output"
+  exit 1
+fi
+
+echo
+echo "Teste 4 realizado com sucesso"
+
+exit 0
